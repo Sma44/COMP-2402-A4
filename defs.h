6647d819b989521a559ec2c900e06d46042564da
Sma44 <samsonma44@gmail.com>
2024-03-21 00:44:11 -0400

    relocated defines

--- defs.h
+++ defs.h
@@ -13,19 +13,17 @@ using namespace std;
 #define MAX_HEALTH    20
 #define MAX_ARR      256
 
-#define SNORC_SPAWN   90
+#define SNORC_SPAWN   90 
 #define MAX_SNORCS    12
 
-#define HERO_SPAWN    2
-#define SNORC_HEIGHT  12
-#define MIN_STRENGTH  2
-#define NO_SPAWN      9
+#define NO_SPAWN      10 
+#define MAX_HEROES    2  
+#define MAX_HEIGHT    10
+#define MIN_STRENGTH  2 
 #define LEFT          0
-#define RIGHT         3
-#define SLOW_CLIMB    5
-#define FAST_CLIMB    6
-#define MAX_CLIMB (MAX_ROW - 7)
-#define FORWARD 1
+#define MAX_CLIMB     11
+#define FORWARD       1
+#define LEFT_BOUND    6
 
 int random(int);
 
